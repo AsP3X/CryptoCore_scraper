d71f516984c2067475206f42a015f4e23b14307b
AsP3X <zombymedia@gmail.com>
2021-11-12 21:54:11 +0100

    TASK: added is running and idle notification

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ const coinMarketCap = require('./bin/fetchCoinMarketCap');
 
 // create intervall that executes every 5 minutes
 
-var minutes = 5, the_interval = minutes * 60 * 1000;
-setInterval(function() {
+function createTimestamp() {
+    const date = new Date();
+    const timestamp = date.getTime();
+    return timestamp;
+}
+
+function createInterval(time) {
+    let minutes = time, interval = minutes * 60 * 1000;
+    return interval;
+}
+
+// var minutes = 5, the_interval = minutes * 60 * 1000;
+const dataCollector = setInterval(function() {
     coinMarketCap.scrape("bitcoin");
-}, the_interval);
+}, createInterval(5));
+
+let counter = 0;
+const minuteTimer = setInterval(() => {
+    counter++;
+    console.log(`IDLE: waiting for execution Minute ${counter} out of 5`);
+
+    if (counter === 4) {
+        counter = 0;
+    }
+        
+}, createInterval(1));
+
+console.log(`WebScraper is now running [${createTimestamp()}]`);
