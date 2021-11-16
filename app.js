fb792b7de87aa1fb34a54268dc5cd0576b9e07e2
AsP3X <schockcrafer@gmail.com>
2021-11-16 17:07:26 +0100

    TASK: added more debuggin functionality

--- app.js
+++ app.js
@@ -18,14 +18,36 @@ function createInterval(time) {
     return interval;
 }
 
-if (args[0] === '--help') {
-    console.log('Usage: node app.js');
-    console.log('Parameter: [--help], [--test-run]');
-} else if (args[0] === '--test-run') {
+function emulateRuntime(coinconfig) {
     for (let i = 0; i < coinconfig.currencies.length; i++) {
         coinMarketCap.scrape(coinconfig.currencies[i]);
     }
-} else {
+}
+
+function emulateLive(time){
+    if (!time) {
+        console.error('Please provide a valid time in minutes');
+    }
+    const interval = createInterval(time);
+
+    let counter = 0;
+    const runtimeLoop = setInterval(() => {
+        counter++;
+        console.log(`IDLE: waiting for execution Minute ${counter} out of 5`);
+    
+        if (counter === 5) {
+            counter = 0;
+            for (let i = 0; i < coinconfig.currencies.length; i++) {
+                coinMarketCap.scrape(coinconfig.currencies[i]);
+            }
+        }
+            
+    }, interval);
+    console.log(`WebScraper is now running [${createTimestamp()}]`);
+    console.log(`Emulating live: with a "${time} minute" interval`);
+}
+
+function executeLive(coinconfig) {
     let counter = 0;
     const runtimeLoop = setInterval(() => {
         counter++;
@@ -40,4 +62,15 @@ if (args[0] === '--help') {
             
     }, createInterval(1));
     console.log(`WebScraper is now running [${createTimestamp()}]`);
+}
+
+if (args[0] === '--help') {
+    console.log('Usage: node app.js');
+    console.log('Parameter: [--help], [--test-run], ([--emulate-live] [interval])');
+} else if (args[0] === '--test-run') {
+    emulateRuntime(coinconfig);
+} else if (args[0] ==='--emulate-live') {
+    emulateLive(args[1]);
+} else {
+    executeLive(coinconfig);
 }
