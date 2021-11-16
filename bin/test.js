f6d76eefca45f626eec23e0dbb73b59446b30f87
AsP3X <schockcrafer@gmail.com>
2021-11-16 16:11:04 +0100

    NON

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,4 @@
 const cdate = require('./modules/cDate');
 const date = cdate.getDate();
 
-console.log(`Today is the: ${date.day}.${date.month}.${date.year}`);
+// console.log(`Today is the: ${date.day}.${date.month}.${date.year}`);
