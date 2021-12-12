2a3a19f4dec8c45719c79b7020ad79a0d6e789cb
AsP3X <zombymedia@gmail.com>
2021-12-12 21:16:29 +0100

    TASK: add compile dataTree file

--- bin/modules/utils/dataTree.js
+++ bin/modules/utils/dataTree.js
@@ -19,4 +19,41 @@ dataTree.saveDirTree = (filePath, dataPath) => {
     }
 }
 
+dataTree.compileData = (dataPath) => {
+    if (fs.existsSync(dataPath)) {
+        
+        const fileContent = fs.readFileSync('dataTree.json', 'utf8');
+        const dataTree = JSON.parse(fileContent);
+        
+        let dataObject = {};
+        
+        
+        dataTree.children.forEach((cryptos) => {
+            dataObject[cryptos.name] = {};
+        
+            cryptos.children.forEach((year) => {
+                dataObject[cryptos.name][year.name] = {};
+        
+                year.children.forEach((month) => {
+                    dataObject[cryptos.name][year.name][month.name] = {};
+        
+                    month.children.forEach((day) => {
+                        dataObject[cryptos.name][year.name][month.name][day.name] = [];
+        
+                        day.children.forEach((items) => {
+                            dataObject[cryptos.name][year.name][month.name][day.name].push(items.name);
+                        });
+                    });
+                });
+            });
+        });
+
+        fs.writeFileSync(dataPath, JSON.stringify(dataObject));
+
+    } else {
+        console.log('Data path does not exist: ' + dataPath);
+    }
+}
+
+
 module.exports = dataTree;
